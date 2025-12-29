99604cb1e3791afb9a947c01d1ca031c226f9bec
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-12-29 20:32:08 +0700

    up bai1

---
+++ data_base/bai1_qlSinhVien.sql
@@ -0,0 +1,17 @@
+create database ql_sinhvien;
+
+create table class(
+	id int primary key,
+    name nvarchar(50)
+);
+
+create table teacher(
+	id int primary key,
+    name nvarchar(50),
+    age int,
+    country nvarchar(50)
+);
+
+select * from class;
+select * from teacher;
+
